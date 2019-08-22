1550ffaffa8940f478b3fa69000b59f74658cc98
Adam Wong <awong017@ucr.edu>
2019-08-22 16:23:15 -0700

    destructured config.js in server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const app = require('./app')
-const PORT = require('./config')
+const { PORT } = require('./config')
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
