4cc579558d53cabf500226a05423a2888bbce931
Adam Wong <awong017@ucr.edu>
2020-02-04 19:33:08 -0800

    prepared server for deployment on Heroku

--- src/app.js
+++ src/app.js
@@ -9,7 +9,6 @@ const goalRouter = require('./goals/goal-router')
 const expenseRouter = require('./expenses/expense-router')
 const budgetRouter = require('./budgets/budget-router')
 const categoryRouter = require('./categories/category-router')
-const uuid = require('uuid/v4')
 
 const app = express()
 
@@ -21,84 +20,6 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 app.use(express.json())
-
-state = [
-  {
-    id: 1,
-    name: 'awong017',
-    password: 'hello'
-  },
-  {
-    id: 2,
-    name: 'clara94',
-    password: 'bun'
-  },
-  {
-    id: 3,
-    name: 'phanman',
-    password: 'fck'
-  }
-];
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.get('/state', (req, res) => {
-  res
-    .status(200)
-    .json(state)
-})
-
-app.post('/state', (req, res) => {
-  const { name, password } = req.body
-
-  if(!name) {
-    return res
-      .status(400)
-      .send('Please provide a name')
-  }
-
-  if(!password) {
-    return res
-      .status(400)
-      .send('Please provide a password')
-  }
-
-  const newUser = {
-    id: uuid(),
-    name: name,
-    password: password
-  }
-
-  state.push(newUser)
-
-  res
-    .status(200)
-    .json(newUser)
-})
-
-app.delete('/state/:user_id', (req, res) => {
-  const { user_id } = req.params;
-
-  const findUser = state.some((user) => {
-    return user.id === parseInt(user_id)
-  })
-
-  if(findUser === false) {
-    return res
-      .status(404)
-      .send(`User with id of ${user_id} not found`)
-  }
-
-  const filteredUsers = state.filter((user) => {
-    return user.id !== parseInt(user_id)
-  })
-
-  res
-    .status(200)
-    .json(filteredUsers)
-})
 
 app.use(function errorHandler(error, req, res, next) {
     let response
